a646d35d6272a03b87980f42c235e3ce84c345f0
Narasimha-bachu <147078029+Simha-bachu@users.noreply.github.com>
2023-11-20 16:59:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -57,88 +57,40 @@ def upload():
 
     return redirect(url_for('login'))
 
-
-# function for getting column info from a DataFrame
-def get_column_info(df):
-    column_names = df.columns.tolist()
-    data_types = df.dtypes.tolist()
-    num_columns = df.shape[1]  # Get the number of columns
-    #data_size = df.shape
-    missing_values = df.isnull().sum().tolist()
-    print("Column Names:", column_names)
-    print("Data Types:", data_types)
-    print("Number of Columns:", num_columns)
-    print("Missing Values:", missing_values)
-    return column_names, data_types, num_columns, missing_values
-
-
-def get_json_column_info(json_data):
+def get_file_info(file_path):
     column_info = []
 
-    if isinstance(json_data, dict):
-        for key, value in json_data.items():
-            dtype = type(value).__name__ if value is not None else 'Unknown'
-            size = 1
-            missing = 0 if value is not None else 1
+    if file_path.endswith('.csv'):
+        df = pd.read_csv(file_path)
+        column_info = [{'col': col, 'dtype': str(dtype), 'size': df[col].size, 'missing': df[col].isnull().sum()}
+                       for col, dtype in zip(df.columns, df.dtypes)]
 
-            column_info.append({
-                'col': key,
-                'dtype': dtype,
-                'size': size,
-                'missing': missing
-                  })
+    elif file_path.endswith('.json'):
+        with open(file_path, 'r') as json_file:
+            json_data = json.load(json_file)
+        column_info = [{'col': key, 'dtype': type(value).__name__, 'size': 1, 'missing': 0 if value is not None else 1}
+                       for key, value in json_data.items()]
+
+    elif file_path.endswith('.xml'):
+        with open(file_path, 'r') as xml_file:
+            soup = BeautifulSoup(xml_file, 'xml')
+        column_info = [{'name': element.name, 'text': element.text.strip() if element.text else None}
+                       for element in soup.find_all()]
+
     return column_info
 
-def get_xml_column_info(xml_data):
-    column_info = []
-
-    soup = BeautifulSoup(xml_data, 'xml')
-
-    for element in soup.find_all():
-        #  info based on the element name
-        name = element.name
-        text = element.text.strip() if element.text else None
-        print(f"Name: {name}, Text: {text}")  # debug line
-
-        column_info.append({
-            'name': name,
-            'text': text,
-        })
-    print("Column Info:", column_info)  # debug line
-    return column_info
-
-
-
-# Route to show information about the uploaded  file
 @app.route('/get_info')
 def file_info():
     if 'username' in session and 'uploaded_data_file_path' in session:
         file_path = session['uploaded_data_file_path']
         
         if os.path.exists(file_path):
-            if file_path.endswith('.csv'):
-                df = pd.read_csv(file_path)
-                column_info = get_column_info(df)
-                print("Column Info:", column_info)
-                return render_template('showdata.html', column_info=column_info)
-            elif file_path.endswith('.json'):
-                 # Handling .json file
-                with open(file_path, 'r') as json_file:
-                    json_data = json.load(json_file)
-                column_info = get_json_column_info(json_data)
-                return render_template('showdata_json.html',column_info=column_info)
-            elif file_path.endswith('.xml'):
-                # Handling .xml file using BeautifulSoup
-                with open(file_path, 'r') as xml_file:
-                    xml_data = xml_file.read()
-                column_info = get_xml_column_info(xml_data)
-                print("Column Info:", column_info)  
-                return render_template('showdata_xml.html', column_info=column_info)
-            else:
-                return "Unsupported file format. Only CSV, JSON, and XML files are allowed."
-
+            column_info = get_file_info(file_path)
+            return render_template('showdata.html', column_info=column_info)
+        
         return "Invalid file path."
-    return redirect(url_for('login'))
+    return "User not authenticated or no file uploaded."
+
 
 @app.route('/dashboard')
 def dashboard():
